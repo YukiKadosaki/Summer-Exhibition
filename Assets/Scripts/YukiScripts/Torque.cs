1f96ea2210edd031f64cd35c0cda003bd744b320
YukiKadosaki <61261945+YukiKadosaki@users.noreply.github.com>
2020-05-23 21:26:34 +0900

    probuilder and progrids
    imported probuilder and progrids.
    make Torque script.

---
+++ Assets/Scripts/YukiScripts/Torque.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torque : MonoBehaviour
+{
+    public Vector3 torque;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(torque * Time.deltaTime);
+    }
+}
