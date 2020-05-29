4ce68ff0ce2ddaa2eb1b2c5271ade2d9683da4a8
katoL6121 <65657609+katoL6121@users.noreply.github.com>
2020-05-29 15:48:03 +0900

    5/29

---
+++ Assets/Scripts/KatoScripts/death.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class death : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+ 
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Finish")
+       
+        {
+            
+        // Debug.Log("GameOver");
+        }
+   
+    }
+}
