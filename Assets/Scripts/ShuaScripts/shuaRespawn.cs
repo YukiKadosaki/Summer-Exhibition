8d3004914b19c3b2002e8419bee3b95422a3b324
shua1ka <shadowmame10@gmail.com>
2020-05-29 23:03:45 +0900

    mizuuuuu
    mizuniotitararisupo-nnsimasuyo

---
+++ Assets/Scripts/ShuaScripts/shuaRespawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shuaRespawn : MonoBehaviour
+{
+    Vector3 initpos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initpos = transform.position;
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.tag == "Water")
+        {
+            transform.position = initpos;
+        }
+    }
+}
