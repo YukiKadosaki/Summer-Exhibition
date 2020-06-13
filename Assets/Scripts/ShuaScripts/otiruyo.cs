ff69e6e44f7d6ad1cc6bcf08dc6f4db4096192da
shua1ka <shadowmame10@gmail.com>
2020-06-13 21:07:33 +0900

    juuryoku
    dekitenaiyo

---
+++ Assets/Scripts/ShuaScripts/otiruyo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class otiruyo : MonoBehaviour
+{
+    Rigidbody rib;
+
+    private void Start()
+    {
+        rib = GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        rib.useGravity = false;
+
+        if (collision.gameObject.tag == "Player")
+        {
+            rib.useGravity = true;
+        }
+    }
+}
