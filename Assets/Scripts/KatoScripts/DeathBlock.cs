9efa69b37985062dc138fd6e1f77b8d32a5133fb
katoL6121 <65657609+katoL6121@users.noreply.github.com>
2020-07-05 19:59:00 +0900

    stage3FIN

---
+++ Assets/Scripts/KatoScripts/DeathBlock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBlock : MonoBehaviour
+{
+    private Vector3 returnpos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeReturnPosition(transform.position);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+
+        if (hit.gameObject.tag == "Death")
+        {
+
+            transform.position = returnpos;
+        }
+    }
+
+    public void ChangeReturnPosition(Vector3 pos)
+    {
+        returnpos = pos;
+    }
+}
---
+++ Assets/Scripts/KatoScripts/yoko.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel.Design;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class yoko : MonoBehaviour
+{
+    private Rigidbody rigid;
+    private Vector3 defaultPos;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        defaultPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigid.MovePosition(new Vector3(defaultPos.x + Mathf.PingPong(Time.time, 3) * speed, defaultPos.y , defaultPos.z));
+    }
+}
