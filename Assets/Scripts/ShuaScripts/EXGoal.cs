ca7c9014d1ec8135d94da6bb0d2aeb46d28995b5
shua1ka <shadowmame10@gmail.com>
2020-07-09 20:05:55 +0900

    shuusei
    yattane

---
+++ Assets/Scripts/ShuaScripts/EXGoal.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class EXGoal : MonoBehaviour
+{
+    GameObject obj;
+    GameObject ex;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        GameObject obj = GameObject.Find("cong");
+
+        if (collider.gameObject.tag == "Goal2")
+        {
+            GameObject ex = GameObject.Find("exgoal");
+            ex.GetComponent<AudioSource>().Play();
+            Invoke("Praise", 3);
+            obj.GetComponent<TMP_Text>().text = "Congratulations";
+        }
+    }
+
+    public void Praise()
+    {
+        GameObject obj = GameObject.Find("cong");
+
+        obj.GetComponent<TMP_Text>().fontSize = 22;
+        obj.GetComponent<TMP_Text>().text = "You are\nwonderful player!";
+        Invoke("Change", 3);
+    }
+
+    public void Change()
+    {
+        GetComponent<ChangeScenes>().Change();
+    }
+}
