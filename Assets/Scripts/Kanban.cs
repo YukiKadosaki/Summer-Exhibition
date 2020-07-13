0881b9a58bbd45b47bed4e0842c1ec545fa7967e
YukiKadosaki <61261945+YukiKadosaki@users.noreply.github.com>
2020-07-13 19:03:57 +0900

    Easy Stage Select
    Press 1~6, 0, 9 or LoadScene

---
+++ Assets/Scripts/Kanban.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kanban : MonoBehaviour
+{
+    private GameObject player;
+    public GameObject text;
+    public string message;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(player.transform.position, this.transform.position) <= 10){
+            text.GetComponent<TextMesh>().text = message;
+        }
+        else
+        {
+            text.GetComponent<TextMesh>().text = "";
+        }
+    }
+}
---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleporter : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene("Stage1");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene("Stage2");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene("Stage3");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SceneManager.LoadScene("Stage4");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            SceneManager.LoadScene("stage5");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            SceneManager.LoadScene("stage6");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            SceneManager.LoadScene("TitleScene");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            SceneManager.LoadScene("EndStage");
+        }
+
+    }
+}
